83c353548948971c45dfa5548b47fef75e70c2e0
Hrant Khachatrian <hrant@egern.net>
2018-03-21 22:39:55 +0400

    Add the skeleton for the neural pipeline

---
+++ iob_to_bind_json.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+
+from __future__ import division
+from __future__ import print_function
+# from __future__ import absolute_import
+
+import codecs
+import argparse
+import json
+import itertools
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_text', '-it', required=True, type=str)
+    parser.add_argument('--input_iob2', '-i', required=True, type=str)
+    parser.add_argument('--output_json', '-o', required=True, type=str)
+    parser.add_argument('--clean_entities', action='store_true')
+    args = parser.parse_args()
+    
+    protein_labels = ['proteingene', 'smallmolecule']
+    
+    print("Read {input_iob2:} IOB2 file and extract entities marked as '{entities:}', store the results to {output_json:}, use sentences from {input_text:}".format(
+        input_iob2=args.input_iob2,
+        output_json=args.output_json,
+        entities="' or '".join(protein_labels),
+        input_text=args.input_text))
+
+    
+    def clean(w):
+        # TODO: replace this with better tokenizer?!
+        if w[-1] in [',', '.', ')', ']', '?', '!']:
+            w = w[:-1]
+        return w
+
+    def generate_all_tuples(proteins):
+        if args.clean_entities:
+            proteins = [clean(p) for p in proteins]
+        proteins = set(proteins)
+        return [{
+            "participant_a": a, 
+            "participant_b": b, 
+            "label": 0, 
+            "interaction_type": "bind"
+           } for a, b in itertools.combinations(proteins, 2)]
+
+    curr_proteins = []
+    curr_word = ''
+    passage = ''
+    output = []
+    bind = False
+    
+    sentences = {}
+    with codecs.open(args.input_text, encoding='utf-8') as file:
+        for line in file:
+            id, sentence = line.split('\t')
+            sentences[id] = sentence
+
+    with codecs.open(args.input_iob2, encoding='utf-8') as file:
+        for line in file:
+            if line == '\n':
+                if curr_word:
+                    curr_proteins.append(curr_word)
+                    curr_word = ''
+                if len(curr_proteins) > 1: 
+                    all_tuples = generate_all_tuples(curr_proteins)
+                else:
+                    all_tuples = []
+                
+                output.append({
+                    "entities": curr_proteins, 
+                    "extracted_information": all_tuples, 
+                    "id": id, 
+                    "text": sentences[id]
+                })
+                    
+                #bind = False
+                curr_proteins = []
+                passage = ''
+                continue
+
+            try:
+                id, _, _, word, label = line.split()
+            except:
+                print("ERROR")
+                print(line)
+
+            passage += ' '
+            passage += word
+
+#            if word in ['bind', 'binding', 'bound', 'binds']:
+#                bind = True
+            t = label[0]
+            if t == 'O':
+                if curr_word:
+                    curr_proteins.append(curr_word)
+                    curr_word = ''
+                continue
+            label = label[2:]
+            if t == 'I' and curr_word:
+                curr_word += ' ' + word
+            else:
+                if curr_word:
+                    curr_proteins.append(curr_word)
+                    curr_word = ''
+                if label in protein_labels:
+                    curr_word = word
+
+    with codecs.open(args.output_json, 'w', encoding='utf-8') as fd:
+        json.dump(output, fd, indent=True)
+
+
+if __name__ == '__main__':
+    main()
---
+++ pipeline_tagNERv2_camr_neural.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from __future__ import division
+# from __future__ import absolute_import
+
+import codecs
+import argparse
+
+from os import path, makedirs
+from subprocess import check_call
+
+
+def ensure_dir(dir):
+    try:
+        makedirs(dir)
+    except OSError:
+        pass
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_text', '-i', required=True, type=str)
+    parser.add_argument('--output_json', '-o', required=True, type=str)
+    parser.add_argument('--tmp_dir', '-t', default='tmp', type=str)
+    parser.add_argument('--classifier_model', '-c', default='', type=str)
+    args = parser.parse_args()
+
+    basename = path.basename(args.input_text)
+    tokenized_input = path.join(args.tmp_dir, '{}.tokenized.txt'.format(basename))
+    entities_output = path.join(args.tmp_dir, '{}.tokenized.txt.IOB'.format(basename))
+    candidate_tuples_json = path.join(args.tmp_dir, '{}.candidates.json'.format(basename))
+    
+    ensure_dir(args.tmp_dir)  # not very necessary
+    
+    print("Tokenizing...")
+    # print("Adding spaces around -")
+    with codecs.open(args.input_text, encoding='utf-8') as fr:
+        with codecs.open(tokenized_input, 'w', encoding='utf-8') as fw:
+            for line in fr.readlines():
+                id, sentence = line[:-1].split('\t')  # \n symbol
+                #sentence = sentence.replace('-',' - ')
+                sentence = ' '.join(sentence.split())
+                fw.write("{}\t{}\n".format(id, sentence))
+
+    print('Running NER...')
+    check_call(['bash', 'tag_NER.sh',
+          '-i', path.join('../..', args.input_text), # because cwd gets one level below
+          '-f', 'IOB'], cwd='submodules/tag_NER_v2')
+    print('Done\n')
+    # the output is entities_output
+    
+    
+    print('Building interaction tuples with unknown labels...')
+    check_call(['python', 'iob_to_bind_json.py',
+          '--input_text', args.input_text,
+          '--input_iob2', entities_output,
+          '--output_json', args.output_json]) #candidate_tuples_json])
+    print('Done\n')
+    
+    print('Adding AMRs')
+    check_call(['python', 'add_amr.py',
+          '--input_json', args.output_json,
+          '--model', 'amr2_bio7_best_after_2_fscore_0.6118.m',
+          '--output_json', args.output_json])
+    print('Done\n')
+    
+    print('Extracting AMR paths')
+    check_call(['python', 'extract_amr_paths.py',
+          '--input_json', args.output_json,
+          '--output_json', args.output_json])
+    print('Done\n')
+    
+    
+
+    # add command to generate SDG .. paths
+    
+    raise Exception("Classifier is not ready!")
+
+    print('Detecting true interactions...')
+    check_call(['python',
+          'submodules/RelationClassification/inference.py',
+          '--input_json', candidate_tuples_json,
+          '--output_json', args.output_json,
+          '--model_path', args.classifier_model
+          ])
+    print('Done\n')
+
+    # replace protein names with identifiers
+
+    # TODO: remove tmp folder?
+
+
+if __name__ == '__main__':
+    main()
