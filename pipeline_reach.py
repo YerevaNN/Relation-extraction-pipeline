b36c588bf00ad601191a899a5caa6c9ef703cf89
Hrant Khachatrian <hrant@egern.net>
2019-03-04 15:57:32 +0400

    REACH pipeline: support new format with more information

--- pipeline_reach.py
+++ pipeline_reach.py
@@ -51,8 +51,8 @@ def main():
             # TODO: understand how to parse fries
             while True:
                 try:
-                    # response_index = requests.post('http://agathon.sista.arizona.edu:8080/odinweb/api/text', 
-                      #                             params={'text': sentence, 'output': 'indexcard'})
+                    response_index = requests.post('http://agathon.sista.arizona.edu:8080/odinweb/api/text',
+                                                  params={'text': sentence, 'output': 'indexcard'})
                     response_fries = requests.post('http://agathon.sista.arizona.edu:8080/odinweb/api/text', 
                                                params={'text': sentence, 'output': 'fries'})
                     break
@@ -63,30 +63,82 @@ def main():
                     pass
                 
             try:
-                data_indexcard = {} #json.loads(response_index.content)
+                data_indexcard = json.loads(response_index.content)
                 data_fries = json.loads(response_fries.content)
-                
-                if 'cards' in data_indexcard:
-                    pairs_from_json = [{
-                        "participant_a": card['extracted_information']['participant_a']['entity_text'], 
-                        "participant_b": card['extracted_information']['participant_b']['entity_text'], 
-                        "label": 1, 
-                        "interaction_type": "bind"
-                    } for card in data_indexcard['cards']
-                        if card['extracted_information']['interaction_type'] == 'binds']
-                else:
-                    pairs_from_json = []
-                    
+
                 if 'entities' in data_fries:
-                    entities_from_json = [x['text'] for x in data_fries['entities']['frames']]
+                    entity_mentions = [{
+                        "name": x['text'],
+                        "label": x['type'],
+                        "mention": [x['start-pos']['offset'], x['end-pos']['offset']],
+                        "grounding": x['xrefs'][0]['namespace'] + ':' + x['xrefs'][0]['id']
+                    } for x in data_fries['entities']['frames']]
                 else:
-                    entities_from_json = []
-                
+                    entity_mentions = []
+
+                entities = {}
+                for mention in entity_mentions:
+                    if mention['name'] not in entities:
+                        entities[mention['name']] = {
+                            "labels_major": set(),
+                            "mentions": [],
+                            "exists": True,
+                            "grounding": set()
+                        }
+                    entities[mention['name']]["labels_major"].add(mention['label'])
+                    entities[mention['name']]["mentions"].append(mention['mention'])
+                    entities[mention['name']]["grounding"].add(mention['grounding'])
+
+                # convert set to list
+                for e, e_obj in entities.items():
+                    e_obj["labels_major"] = list(e_obj["labels_major"])
+                    e_obj["grounding"] = list(e_obj["grounding"])
+                    if len(e_obj["grounding"]) > 1:
+                        print("\nMultiple groundings for the same entity {} (ID={})".format(e, id))
+                    e_obj["grounding"] = e_obj["grounding"][0]
+
+                unique_entities = {i: {
+                    "versions": {e:e_obj},
+                    "labels_major": e_obj['labels_major'],
+                    "all_implicit": False,
+                    "grounding": e_obj["grounding"]
+                } for i, (e, e_obj) in enumerate(entities.items())}
+
+                pairs_from_json = []
+                if 'cards' in data_indexcard:
+                    for card in data_indexcard['cards']:
+                        if card['extracted_information']['interaction_type'] != 'binds':
+                            continue
+                        a_grounding = card['extracted_information']['participant_a']['identifier']
+                        b_grounding = card['extracted_information']['participant_b']['identifier']
+                        a_ids = [ue for ue, ue_obj in unique_entities.items() if ue_obj['grounding'] == a_grounding]
+                        b_ids = [ue for ue, ue_obj in unique_entities.items() if ue_obj['grounding'] == b_grounding]
+                        if len(a_ids) != 1:
+                            print(u"Participant A {}: {} ids found (ID={})".format(
+                                card['extracted_information']['participant_a']['entity_text'],
+                                len(a_ids),
+                                id
+                            ))
+                        if len(b_ids) != 1:
+                            print(u"Participant B {}: {} ids found (ID={})".format(
+                                card['extracted_information']['participant_b']['entity_text'],
+                                len(a_ids),
+                                id
+                            ))
+                        pairs_from_json.append({
+                            "participant_a": card['extracted_information']['participant_a']['entity_text'],
+                            "participant_b": card['extracted_information']['participant_b']['entity_text'],
+                            "participant_ids": [a_ids[0], b_ids[0]],
+                            "label": 1,
+                            "interaction_type": "bind"
+                        })
+
                 output.append({
-                  "entities": entities_from_json, 
-                  "extracted_information": pairs_from_json, 
-                  "id": id, 
-                  "text": sentence
+                    "entities": entities,
+                    "unique_entities": unique_entities,
+                    "extracted_information": pairs_from_json,
+                    "id": id,
+                    "text": sentence
                  })
                 
                 positive_pairs += len(pairs_from_json)
@@ -95,7 +147,7 @@ def main():
                     
             except Exception as e:
                 print(e)
-                print("ERROR: Skipping the sentence: {}".format(line))
+                print(u"ERROR: Skipping the sentence: {}".format(line))
                 
             #print("{} positive pairs found from {} sentences".format(positive_pairs, i+1))
 
